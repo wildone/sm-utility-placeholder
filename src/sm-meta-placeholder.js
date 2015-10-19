225a61b20f6bc0e17abc7ca7255d3c8b8f1c80dc
Bede <bede.overend@gmail.com>
2015-10-20 09:12:01 +1100

    add show, shouldInline properties

--- src/sm-meta-placeholder.js
+++ src/sm-meta-placeholder.js
@@ -1,6 +1,24 @@
 class SmMetaPlaceholder {
   beforeRegister() {
     this.is = 'sm-meta-placeholder';
+
+    this.properties = {
+      show: {
+        type: Boolean,
+        reflectToAttribute: true,
+        value: false
+      },
+      shouldInline: {
+        type: Boolean,
+        observer: '_shouldInlineChanged',
+        value: false
+      }
+    };
+  }
+
+  _shouldInlineChanged(value) {
+    this.customStyle['--display'] = value ? 'inline' : 'block';
+    this.updateStyles();
   }
 }
 
